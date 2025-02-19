97e76655c98587254ca5f37c932686c46dbf8ae2
pablodiazjorge <pablodiazjorgegarcia@gmail.com>
2025-02-19 17:46:50 +0100

    feat: implement book cards to display books in Book component
    - Designed and implemented card layout in `book.component.html` to showcase book details.
    - Bound book data properties (title, author, pages, price) to the card elements.
    - Styled the cards for better visual representation using CSS or PrimeNG components.
    - Ensured proper data binding and rendering of book information within the component.

--- src/app/home-component/home-component.component.ts
+++ src/app/home-component/home-component.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
+import { Book } from '../models/book';
+import { BookService } from '../services/book.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home-component',
-  imports: [ButtonModule, CardModule],
+  imports: [ButtonModule, CardModule, RouterModule],
   templateUrl: './home-component.component.html',
   styleUrl: './home-component.component.scss'
 })
 export class HomeComponentComponent {
+  books: Book[] = []
 
+  constructor(private bookService: BookService) { }
+
+  ngOnInit(): void {
+
+  }
+
+  getAllBooks() {
+    this.bookService.getBooks().subscribe((data) => {
+      this.books = data;
+    });
+  }
 }
