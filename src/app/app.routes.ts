5a24f96dcb83405c87061b2d0585c60d2c73a0ae
pablodiazjorge <pablodiazjorgegarcia@gmail.com>
2025-02-19 16:51:47 +0100

    feat: implement basic routing and create two new components
    - Set up basic routing in 'app.routes.ts'.
    - Created 'BookFormComponent' for handling book form functionality.
    - Created 'HomeComponent' as the landing page for the application.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { HomeComponentComponent } from './home-component/home-component.component';
+import { BookFormComponentComponent } from './book-form-component/book-form-component.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponentComponent,
+        title:'Home page'
+    },
+    {
+        path: 'book-form-component/:id',
+        component: BookFormComponentComponent,
+        title:'Books formulary'
+    },
+    {
+        path: '**',
+        redirectTo:'',
+        pathMatch:'full'
+    },
+];
---
+++ src/app/book-form-component/book-form-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book-form-component',
+  imports: [],
+  templateUrl: './book-form-component.component.html',
+  styleUrl: './book-form-component.component.scss'
+})
+export class BookFormComponentComponent {
+
+}
