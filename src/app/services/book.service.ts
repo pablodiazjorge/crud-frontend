e5cadcc1960c4c66e0a606ad7613f7e6968356b3
pablodiazjorge <pablodiazjorgegarcia@gmail.com>
2025-02-19 17:10:51 +0100

    feat: implement book model and service for CRUD operations
    - Created `models` folder and added `book.model.ts` with the following structure:
        - 'id': 'number'
        - 'title': 'string'
        - 'author': 'string'
        - 'pages': 'number'
        - 'price': 'number'
    
    - Created `services` folder and added `book.service.ts` with complete CRUD methods:
        - 'getBooks()': Retrieves all books.
        - 'getBookById(id)': Retrieves a book by its ID.
        - 'createBook(book)': Creates a new book.
        - 'updateBook(book)': Updates an existing book.
        - 'deleteBook(id)': Deletes a book by its ID.
    
    - Ensured property names match the backend entity structure for consistency.

---
+++ src/app/services/book.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Book } from '../models/book';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookService {
+  private apiUrl = 'http://localhost:8080/book';
+
+  constructor(private http: HttpClient) { }
+
+  getBooks(): Observable<Book[]> {
+    return this.http.get<Book[]>(this.apiUrl);
+  }
+
+  getBookById(id: number): Observable<Book> {
+    return this.http.get<Book>(`{this.apiUrl}/${id}`);
+  }
+
+  createBook(book: Book) {
+    return this.http.post(this.apiUrl, book);
+  }
+
+  updateBook(book: Book) {
+    return this.http.put(this.apiUrl, book);
+  }
+
+  deleteBook(id: number) {
+    return this.http.delete<Book>(`{this.apiUrl}/${id}`);
+  }
+}
