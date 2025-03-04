deb99e5a56dd7cb89816408c0b720b0ab3bcc5b4
pablodiazjorge <pablodiazjorgegarcia@gmail.com>
2025-03-04 14:59:10 +0100

    feat(book-management): Implement pagination on frontend using PrimeNG
    This commit adds pagination functionality to the Book Management Application's frontend, aligning it with the backend setup. Key changes include:
    
    - Integrated PrimeNG's `p-paginator` component for seamless pagination.
    - Updated the `home-component` template and logic to handle paginated book data.
    - Modified the service methods in `book.service.ts` to support pagination parameters.
    - Adjusted the API calls to fetch books in paginated chunks.
    -Created page model.
    
    These changes improve the user experience by allowing users to navigate through large datasets of books more efficiently.
    
    Files affected:
    - src/app/home-component/home-component.component.html
    - src/app/home-component/home-component.component.ts
    - src/app/services/book.service.ts

--- src/app/home-component/home-component.component.ts
+++ src/app/home-component/home-component.component.ts
@@ -1,33 +1,53 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
+import { PaginatorModule } from 'primeng/paginator';
 import { Book } from '../models/book';
+import { Page } from '../models/page';
 import { BookService } from '../services/book.service';
 import { RouterModule } from '@angular/router';
 import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-home-component',
-  imports: [ButtonModule, CardModule, RouterModule],
+  imports: [ButtonModule, CardModule, RouterModule, PaginatorModule],
   templateUrl: './home-component.component.html',
   styleUrl: './home-component.component.scss'
 })
 export class HomeComponentComponent {
   books: Book[] = []
+  totalRecords: number = 0; // Total books
+  rows: number = 10; // Books per page
+  currentPage: number = 0; // Current Page (0-based)
   isDeleteInProgress: boolean = false;
   constructor(
     private bookService: BookService,
     private messageService: MessageService,
-  ) {}
+  ) { }
 
   ngOnInit(): void {
-    this.getAllBooks();
+    this.loadBooks(this.currentPage, this.rows);
   }
 
-  getAllBooks() {
-    this.bookService.getBooks().subscribe((data) => {
-      this.books = data;
+  /**
+    * Loads books by fetching paginated data from the book service.
+    * 
+    * @param page The page number to load.
+    * @param size The number of items per page.
+  */
+  loadBooks(page: number, size: number): void {
+    this.bookService.getBooks(page, size).subscribe((data: Page<Book>) => {
+      this.books = data.content; // Books current page
+      this.totalRecords = data.totalElements; // Total books
+      this.currentPage = data.number; // Current page
+      this.rows = data.size; // Size page
     });
+  }
+
+  onPageChange(event: any): void {
+    this.currentPage = event.page; // New page selected
+    this.rows = event.rows; // New page size
+    this.loadBooks(this.currentPage, this.rows);
   }
 
   deleteBook(id: number) {
@@ -40,7 +60,7 @@ export class HomeComponentComponent {
           detail: 'Book deleted',
         });
         this.isDeleteInProgress = false;
-        this.getAllBooks();
+        this.loadBooks(this.currentPage, this.rows); // Reload current page
       },
       error: () => {
         this.isDeleteInProgress = false;
--- src/app/services/book.service.ts
+++ src/app/services/book.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Book } from '../models/book';
+import { Page } from '../models/page';
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +12,8 @@ export class BookService {
 
   constructor(private http: HttpClient) { }
 
-  getBooks(): Observable<Book[]> {
-    return this.http.get<Book[]>(this.apiUrl);
+  getBooks(page: number = 0, size: number = 10): Observable<Page<Book>> {
+    return this.http.get<Page<Book>>(`${this.apiUrl}?page=${page}&size=${size}`);
   }
 
   getBookById(id: number): Observable<Book> {
