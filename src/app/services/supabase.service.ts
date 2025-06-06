6d1d93d5342891567e98ce930bd8ff7d3d470929
pablodiazjorge <pablodiazjorgegarcia@gmail.com>
2025-06-06 18:54:53 +0200

    feat: fix problem

--- src/app/services/supabase.service.ts
+++ src/app/services/supabase.service.ts
@@ -1,13 +1,17 @@
-import { Injectable } from '@angular/core';
-import { createClient, SupabaseClient } from '@supabase/supabase-js';
+import { Injectable } from "@angular/core";
+import { createClient, SupabaseClient } from "@supabase/supabase-js";
 
-@Injectable({ providedIn: 'root' })
+@Injectable({ providedIn: "root" })
 export class SupabaseService {
   public supabase: SupabaseClient;
 
   constructor() {
     const supabaseUrl = (window as any)._env_.SUPABASE_URL;
     const supabaseAnonKey = (window as any)._env_.SUPABASE_ANON_KEY;
+
+    if (!supabaseUrl || !supabaseAnonKey) {
+      throw new Error('Supabase environment variables not set');
+    }
 
     this.supabase = createClient(supabaseUrl, supabaseAnonKey);
   }
