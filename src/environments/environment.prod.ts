81fac886dd4f466500393adcb4c4bb5f1c2d7702
pablodiazjorge <pablodiazjorgegarcia@gmail.com>
2025-06-09 10:59:48 +0200

    feat: fix environment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  SUPABASE_URL: 'undefined',
-  SUPABASE_ANON_KEY: 'undefined',
+  SUPABASE_URL: 'NG_APP_SUPABASE_URL',
+  SUPABASE_ANON_KEY: 'NG_APP_SUPABASE_ANON_KEY',
 };
